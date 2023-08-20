662dc7e40a518d963acc2e851382dd40389f44ac
Elena <lenulka.90@mail.ru>
2023-08-20 14:38:27 +0300

    Start.

---
+++ src/main/java/pro/sky/HW_Repeat_Collections/HwRepeatCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HW_Repeat_Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwRepeatCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwRepeatCollectionsApplication.class, args);
+	}
+
+}
