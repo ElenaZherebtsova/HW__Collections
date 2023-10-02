a6ff8c8c71d424eda336243f59b8fee109dab624
Elena <lenulka.90@mail.ru>
2023-10-02 08:18:35 +0300

    Start tests.

---
+++ src/test/java/pro/sky/HW_Repeat_Collections/HwRepeatCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HW_Repeat_Collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwRepeatCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/pro/sky/HW_Repeat_Collections/Service/EmployeeServiceImplTest.java
@@ -0,0 +1,39 @@
+package pro.sky.HW_Repeat_Collections.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+    private EmployeeServiceImpl underTest = new EmployeeServiceImpl();
+
+
+    @Test
+    void addEmployee_shouldAddEmployeeToMapAndReturn() {
+        String firstName = "Kate";
+        String lastName = "Ivanova";
+        int department = 2;
+        double salary = 50_000;
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void getEmployee() {
+    }
+
+    @Test
+    void printAll() {
+    }
+}
